f1b0e49cdb9777de84d10ab892b9218aa4fd7174
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-04-19 23:13:38 +0400

    initial server setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const path=require('path')
+
+const app=express();
+
+
+
+app.use('/',express.static(path.join(__dirname,'public')))
+
+app.use('/',require('./routes/root'));
+
+app.all('*',(req,res)=>{
+    res.status(404)
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname,'views','404.html'));
+    }else if(req.accepts('json')) {
+        res.json({message:'Resource not found'})
+    }else{
+        res.type('txt').send('Not Found')
+    }
+})
+
+const PORT=process.env.PORT || 3500;
+
+app.listen(PORT,()=>console.log(`Server running at port ${PORT}`))
