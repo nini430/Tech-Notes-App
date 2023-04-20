defd97d6ffa3c2324e49317b7717c2e18b190778
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-04-20 11:44:52 +0400

    add mongo models

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const path=require('path')
 
 const express=require('express')
 const cookieParser=require('cookie-parser')
 const cors=require('cors');
+const mongoose=require('mongoose')
 
 const corsOptions=require('./config/corsOptions')
-const {logger}=require('./middleware/logger')
+const {logger,logEvent}=require('./middleware/logger')
 const errorHandler=require('./middleware/errorHandler')
+const connectDB=require('./config/dbConn')
+
 
 const app=express();
+
+connectDB();
 
 app.use(logger);
 app.use(cors(corsOptions))
@@ -32,4 +38,14 @@ app.all('*',(req,res)=>{
 const PORT=process.env.PORT || 3500;
 
 app.use(errorHandler);
-app.listen(PORT,()=>console.log(`Server running at port ${PORT}`))
+
+mongoose.connection.once('open',()=>{
+    console.log('MongoDB connection ready')
+    app.listen(PORT,()=>console.log(`Server running at port ${PORT}`))
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log(err);
+    const message=`${err.no}- ${err.code}\t${err.syscall}\t${err.hostname}`;
+    logEvent(message,'mongoError.log');
+})
